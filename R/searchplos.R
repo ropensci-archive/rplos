7b9032b2789c4a693e98422f6ad96ea6fa278a16
Scott Chamberlain <myrmecocystus@gmail.com>
2011-07-02 23:07:14 -0500

    changed searchplos fxn to get all results if asked for, getting around 999 limit pulldown

--- R/searchplos.R
+++ R/searchplos.R
@@ -7,7 +7,9 @@ searchplos <-
 #     any combination of search fields [see plosfields$field] 
 #   limit: number of results to return (integer)
 # Examples:
-#   searchplos('drosophila', 'id', 20)
+#   searchplos('ecology', 'id', 2)
+#   searchplos('ecology', 'id', 1001)
+#   searchplos('ecology', 'id,title', 2)
 
 function(terms, fields = NA, limit = NA, 
   url = 'http://api.plos.org/search',
@@ -20,17 +22,46 @@ function(terms, fields = NA, limit = NA,
     args$q <- terms
   if(!is.na(fields))
     args$fl <- fields
-  if(!is.na(limit))
-    args$rows <- limit
   args$wt <- "json"
-  tt <- getForm(url, 
-    .params = args,
-    ...,
+  
+  argsgetnum <- args
+  argsgetnum$rows <- 0
+  getnum <- getForm(url, 
+    .params = argsgetnum,
+#     ...,
     curl = curl)
-  jsonout <- fromJSON(I(tt))
-  tempresults <- jsonout$response$docs
-  numres <- length(tempresults) # number of search results 
-  names(numres) <- 'Number of search results'
-  dfresults <- data.frame( do.call(rbind, tempresults) )
-  return(list(numres, dfresults))
+  getnumrecords <- fromJSON(I(getnum))$response$numFound
+  
+  if(min(getnumrecords, limit) < 1000) {
+    if(!is.na(limit))
+      args$rows <- limit
+    tt <- getForm(url, 
+      .params = args,
+#     ...,
+      curl = curl)
+    jsonout <- fromJSON(I(tt))
+    tempresults <- jsonout$response$docs
+    numres <- length(tempresults) # number of search results 
+    names(numres) <- 'Number of search results'
+    dfresults <- data.frame( do.call(rbind, tempresults) )
+    return(list(numres, dfresults))  
+  } else
+    { 
+      gotothis <- min(getnumrecords, limit)
+      getvecs <- seq(from=1, to=gotothis, by=500)
+      args$rows <- 500
+      dfresults_ <- list()
+      for(i in 1:length(getvecs)) {
+        args$start <- i
+        tt <- getForm(url, 
+          .params = args,
+#         ...,
+          curl = curl)
+        jsonout <- fromJSON(I(tt))
+        tempresults <- jsonout$response$docs  
+        dfresults_[[i]] <- data.frame( do.call(rbind, tempresults) )
+      }
+      dfresults__ <- do.call(rbind, dfresults_)
+      return(list(getnumrecords, dfresults__))
+    }
 }
