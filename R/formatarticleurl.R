504f90e0a87f7070d723c5eedbf2fdbc56b72253
Scott Chamberlain <myrmecocystus@gmail.com>
2011-12-11 18:09:01 -0600

    edited man pages

--- R/formatarticleurl.R
+++ R/formatarticleurl.R
@@ -2,13 +2,25 @@
 #' @param doi digital object identifier for an article in PLoS Journals
 #' @param journal quoted journal name (character)
 #' @return Get url for the article to use in your browser, etc.
+#' @details Choose the journal abbreviation, and the appropriate base URL
+#'    will be inclued in the built URL.  Options are:
+#'    PLoSBiology, PLoSGenetics, PLoSComputationalBiology, PLoSMedicine,
+#'	  PLoSONE, PLoSNeglectedTropicalDiseases, or PLoSPathogens.
 #' @export
 #' @examples
 #' formatarticleurl("10.1371/journal.pone.0004045", 'PLoSONE')
 formatarticleurl <- 
 
-function(doi, journal) 
-{
+function(doi, journal) {  
+  journalUrls <- c(  # string with all journal base url's
+    PLoSBiology = 'http://www.plosbiology.org',
+    PLoSGenetics = 'http://www.plosgenetics.org', 
+    PLoSComputationalBiology = 'http://www.ploscompbiol.org',
+	  PLoSMedicine = 'http://www.plosmedicine.org',
+	  PLoSONE = 'http://www.plosone.org',
+	  PLoSNeglectedTropicalDiseases = 'http://www.plosntds.org',
+	  PLoSPathogens = 'http://www.plospathogens.org'
+  )
   paste(journalUrls[journal], '/article/info%3Adoi%2F', 
     str_replace(doi, '/', '%2F'), sep="")
 }
