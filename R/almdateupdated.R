a02242b06250f1b2569460686d1ce5030faf2564
Karthik Ram <karthik.ram@gmail.com>
2012-02-22 14:15:10 -0800

    Fixed bug that broke the function.

--- R/almdateupdated.R
+++ R/almdateupdated.R
@@ -5,7 +5,7 @@
 #' @param key your PLoS API key, either enter, or loads from .Rprofile
 #' @param url the PLoS API url for the function (should be left to default)
 #' @param ... optional additional curl options (debugging tools mostly)
-#' @param curl If using in a loop, call getCurlHandle() first and pass 
+#' @param curl If using in a loop, call getCurlHandle() first and pass
 #'  the returned value in here (avoids unnecessary footprint)
 #' @return Date when article data was last updated.
 #' @export
@@ -13,19 +13,19 @@
 #' almdateupdated('10.1371/journal.pone.0026871')
 #' almdateupdated('10.1371/journal.pone.0026871', 'year')
 #' }
-almdateupdated <- 
+almdateupdated <-
 
 function(doi, get = NA,
   url = 'http://alm.plos.org/articles',
   key = getOption("PlosApiKey", stop("need an API key for PLoS Journals")),
-  ..., 
+  ...,
   curl = getCurlHandle() ) {
-    
+
   url2 <- paste(url, "/", doi, '.json?api_key=', key, sep='')
   tt <- getURLContent(url2)
   date <- fromJSON(I(tt))$article$updated_at
-  date_vector <- str_split(str_split(aa, "T")[[1]][1], "-")[[1]]
-  if(is.na(get) == TRUE) {str_split(aa, "T")[[1]][1]} else
+  date_vector <- str_split(str_split(date, "T")[[1]][1], "-")[[1]]
+  if(is.na(get) == TRUE) {str_split(date, "T")[[1]][1]} else
     if(get == 'year') {as.numeric(date_vector[1])} else
       if(get == 'month') {as.numeric(date_vector[2])} else
         if(get == 'day') {as.numeric(date_vector[3])}
