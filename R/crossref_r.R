e717c660b7f028c35763c09ea34fdf0aea4344e8
Scott Chamberlain <myrmecocystus@gmail.com>
2012-06-21 09:48:44 -0700

    added crossref_r for getting random set of DOI's through crossref
    also updated description and readme

---
+++ R/crossref_r.R
@@ -0,0 +1,54 @@
+#' Get 10 random DOI's through CrossRef.
+#' 
+#' From CrossRef website: "It [this API] might be useful to anybody doing research 
+#'    on scholarly publications."
+#' 
+#' @import RJSONIO
+#' @param count The number of returned random DOIs. Maximum is 1000, default 20. 
+#'    Note that a request for 1000 random DOIs will take a few seconds to 
+#'    complete, whereas a request for 20 will take ~1 second.
+#' @param to Return only DOIs published before and including the given year.
+#' @param from Return only DOIs published after and including the given year. 
+#'    Specifiying a long time period with to and from will cause multi-second 
+#'    response times since the service will try to provide DOIs distributed 
+#'    throughout the time period.
+#' @param type Return DOIs of a certain type. Must be one of these unixref 
+#'    derived types: journal_article, conference_paper, report_paper, 
+#'    journal_issue, journal, book, book_series, book_set, dissertation, 
+#'    content_item, series, or standard.
+#' @param url The Crossref API url for the function (should be left to default)
+#' @param ... optional additional curl options (debugging tools mostly)
+#' @param curl If using in a loop, call getCurlHandle() first and pass 
+#'    the returned value in here (avoids unnecessary footprint)
+#' @return Ten DOI's in R's bibentry format.
+#' @details From the Crossref documentation: A random distribution of values, 
+#'    0 to 1, has been assigned to our DOI records. We use this as an index to 
+#'    look up a random values between 0 and 1 on each request, and take a 
+#'    series of DOIs from that point in randomised order. Selection on year, 
+#'    title and ISSN filters out records before a lookup on the random index.
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @seealso The Crossref documentation for this API: http://random.labs.crossref.org/
+#' @export
+#' @examples \dontrun{
+#' crossref_r()
+#' crossref_r(type = 'journal_article')
+#' }
+crossref_r <- function(count = NA, to = NA, from = NA, type = NA, 
+      url = "http://random.labs.crossref.org/dois", 
+      ..., curl = getCurlHandle())
+{
+  args <- list()
+  if(!is.na(count))
+    args$count <- count
+  if(!is.na(to))
+    args$to <- to
+  if(!is.na(from))
+    args$from <- from
+  if(!is.na(type))
+    args$type <- type
+  tt <- getForm(uri = url, 
+      .params = args,
+      ..., 
+      curl = curl)
+  fromJSON(tt)
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # `rplos`
 
-You can get this package at CRAN here: http://cran.r-project.org/web/packages/rplos/. 
+You can get this package at CRAN [here](http://cran.r-project.org/web/packages/rplos/). 
 
 Another option to install is install_github within Hadley's devtools package.
 
@@ -13,12 +13,18 @@ require(rplos)
 
 This set of functions/package will access full text articles from the Public Library of Science journals using their API. 
 
-Get your PLoS API key here:  http://api.plos.org/
+Get your PLoS API key [here](http://api.plos.org/)
 
 Put your API key in your .Rprofile file using exactly this: 
 options(PlosApiKey = "YOURPLOSAPIKEY"), 
 and the functions within this package will be able to use your API key without you having to enter it every time you run a search. 
 
-rplos tutorial at rOpenSci website here: http://ropensci.org/tutorials/rplos-tutorial/
+rplos tutorial at rOpenSci website [here](http://ropensci.org/tutorials/rplos-tutorial/)
 
-Visit our GitHub hosted website here: http://ropensci.github.com/rplos/
+PLoS API documentation [here](http://api.plos.org/)
+
+Crossref API documentation [here](http://random.labs.crossref.org/) and [here](http://help.crossref.org/#home)
+
+Visit our GitHub hosted website [here](http://ropensci.github.com/rplos/)
+
+rplos is part of the [rOpenSci Project](http://ropensci.github.com)
