ff65b5cfa9b82b9afc9972c2999427aa8d4e42d4
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-21 09:38:09 -0800

    fixed plosfigtabcaps test

--- tests/testthat/test-plosfigtabcaps.R
+++ tests/testthat/test-plosfigtabcaps.R
@@ -4,13 +4,15 @@ context("plosfigtabcaps")
 dat <- plosfigtabcaps(q='ecology', fl='figure_table_caption', limit=10, key = "hello")
 
 test_that("plosfigtabcaps returns the correct dimenion result", {
-	expect_that(nrow(dat), equals(10))
-	expect_that(length(names(dat)), equals(1))
-	expect_that(names(dat), equals("figure_table_caption"))
-	expect_that(names(plosfigtabcaps(q='ecology', fl='id', limit=10, key = "hello")), 
+	expect_that(NROW(dat$data), equals(10))
+	expect_that(length(names(dat$data)), equals(1))
+	expect_that(names(dat$data), equals("figure_table_caption"))
+	expect_that(names(plosfigtabcaps(q='ecology', fl='id', limit=10, key = "hello")$data), 
 							equals("id"))
 })
 
 test_that("plosfigtabcaps returns the correct class", {
-	expect_that(class(dat), is_a("character"))
+	expect_is(dat, "list")
+	expect_is(dat$meta, "list")
+	expect_is(dat$data, "data.frame")
 })
