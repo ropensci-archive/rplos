cf685aed94fe4a304c306b12a4d8555e00a45fd8
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-21 09:34:46 -0800

    fixed plosabstract test

--- tests/testthat/test-plosabstract.R
+++ tests/testthat/test-plosabstract.R
@@ -2,11 +2,11 @@
 context("plosabstract")
 
 test_that("plosabstract returns the correct dimensioned data.frame", {
-	expect_equal(nrow(plosabstract(q = 'drosophila', fl='abstract', limit=10, key = "hello")), 10)
-	expect_equal(nrow(plosabstract(q = 'drosophila', fl='abstract', limit=5, key = "hello")), 5)
+	expect_equal(NCOL(plosabstract(q = 'drosophila', fl='abstract,id', limit=10, key = "hello")$data), 2)
+	expect_equal(NROW(plosabstract(q = 'drosophila', fl='abstract', limit=5, key = "hello")$data), 5)
 })
 
 test_that("plosabstract returns the correct class", {
-	expect_is(plosabstract(q = 'drosophila', fl='author', limit = 5, key = "hello"), "data.frame")
-	expect_is(plosabstract(q = 'Jones', fl='author', limit = 5, key = "hello")[1,1], "character")
+	expect_is(plosabstract(q = 'drosophila', fl='author', limit = 5, key = "hello")$data, "data.frame")
+	expect_is(plosabstract(q = 'Jones', fl='author', limit = 5, key = "hello")$data[1,1], "character")
 })
