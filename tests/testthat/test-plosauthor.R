8ecc27b57b17868519016fdef78d2595ae08fcca
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-21 09:36:16 -0800

    fixed plosauthor test

--- tests/testthat/test-plosauthor.R
+++ tests/testthat/test-plosauthor.R
@@ -2,11 +2,11 @@
 context("plosauthor")
 
 test_that("plosauthor returns the correct dimensions in the data.frame", {
-	expect_equal(nrow(plosauthor(q = 'johnson', fl = 'title,author', limit = 100, key = "hello")), 100)
-	expect_equal(length(names(plosauthor(q = 'johnson', fl = 'title,author', limit = 100, key = "hello"))), 2)
+	expect_equal(NROW(plosauthor(q = 'johnson', fl = 'title,author', limit = 100, key = "hello")$data), 100)
+	expect_equal(length(names(plosauthor(q = 'johnson', fl = 'title,author', limit = 100, key = "hello")$data)), 2)
 })
 
 test_that("plosauthor returns the correct column names", {
-	expect_that(names(plosauthor(q = 'johnson', fl = 'title,author', limit = 100, key = "hello")), 
-							equals(c("author","title")))
+	expect_equal(names(plosauthor(q = 'johnson', fl = 'title,author', limit = 100, key = "hello")$data), 
+               c("author","title"))
 })
