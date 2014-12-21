4405e3d7722e3fe1db5581b36cbe41fdefdce64e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-21 09:40:58 -0800

    fixed searchplos test

--- tests/testthat/test-searchplos.R
+++ tests/testthat/test-searchplos.R
@@ -5,21 +5,23 @@ dat1 <- searchplos('ecology', 'id,publication_date', limit = 2, key = "hello")
 dat2 <- searchplos(q="*:*", fl='id', fq='cross_published_journal_key:PLoSONE', start=0, limit=15, key = "hello")
 
 test_that("searchplos returns the correct dimensions", {
-	expect_that(nrow(dat1), equals(2))
-	expect_that(ncol(dat1), equals(2))
-	expect_that(nrow(dat2), equals(15))
-	expect_that(ncol(dat2), equals(1))
+	expect_that(nrow(dat1$data), equals(2))
+	expect_that(ncol(dat1$data), equals(2))
+	expect_that(nrow(dat2$data), equals(15))
+	expect_that(ncol(dat2$data), equals(1))
 })
 
 test_that("searchplos returns the correct class", {
-  expect_that(dat1[1,1][[1]], is_a("character"))
-  expect_that(dat2[1,1][[1]], is_a("character"))
-	expect_that(dat1, is_a("data.frame"))
-  expect_that(dat2, is_a("data.frame"))
+  expect_that(dat1$data[1,1][[1]], is_a("character"))
+  expect_that(dat2$data[1,1][[1]], is_a("character"))
+	expect_that(dat1$data, is_a("data.frame"))
+  expect_that(dat2$data, is_a("data.frame"))
+  expect_is(dat2, "list")
+  expect_is(dat2$meta, "list")
 })
 
 test_that("searchplos returns the correct value", {
-  expect_that(grepl('10.1371', dat2[1,1]), is_true())
+  expect_that(grepl('10.1371', dat2$data[1,1]), is_true())
 })
 
 test_that("searchplos catches bad limit param", {
