a7c98cf8e7b8928720ade9ab1b0d0d0e54e7dd42
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-22 07:32:47 -0800

    cleaned up plosviews, had old rcurl params, added in ... for curl options

--- R/plosviews.R
+++ R/plosviews.R
@@ -8,9 +8,7 @@
 #'    (character)
 #' @param limit number of results to return (integer)
 #' @param key your PLoS API key, either enter, or loads from .Rprofile
-#' @param callopts Optional additional curl options (debugging tools mostly)
-#' @param curl If using in a loop, call getCurlHandle() first and pass
-#'  the returned value in here (avoids unnecessary footprint)
+#' @param ... Optional additional curl options (debugging tools mostly)
 #' @examples \dontrun{
 #' plosviews('10.1371/journal.pone.0002154', 'id', 'alltime')
 #' plosviews('10.1371/journal.pone.0002154', 'id', 'last30')
@@ -22,10 +20,8 @@
 
 plosviews <- function(search, byfield = NULL, views = 'alltime', limit = NULL,
   key = getOption("PlosApiKey", stop("need an API key for PLoS Journals")),
-  callopts=list(), curl = getCurlHandle())
+  ...)
 {
-	url = 'http://api.plos.org/search'
-
   args <- ploscompact(list(apikey = key, wt = "json", fq = "doc_type:full", rows = limit))
   if(is.null(byfield)) {byfield_ <- byfield} else
     {byfield_ <- paste(byfield, ":", sep="")}
@@ -35,7 +31,7 @@ plosviews <- function(search, byfield = NULL, views = 'alltime', limit = NULL,
     if (views == 'alltime') {args$fl <- 'id,counter_total_all'} else
       if (views == 'last30') {args$fl <- 'id,counter_total_month'} else
         {args$fl <- 'id,counter_total_all,counter_total_month'}
-	tt <- GET(url, query=args, callopts)
+	tt <- GET(pbase(), query=args, ...)
 	stop_for_status(tt)
 	temp <- content(tt)$response$docs
   df <- do.call(rbind, lapply(temp, function(x) data.frame(x)))
