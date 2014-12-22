7321cd3e0955ab04218f486b36570703a786cc5a
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-22 07:25:57 -0800

    fixed up plosword fxn, removed rcurl dep

--- R/plosword.R
+++ R/plosword.R
@@ -1,66 +1,36 @@
 #' Search results on a keyword over all fields in PLoS Journals.
 #'
-#' @import RCurl ggplot2
+#' @export
+#' @import ggplot2
 #' @importFrom plyr ldply
+#' 
 #' @param terms search terms (character)
 #' @param vis visualize results in bar plot or not (TRUE or FALSE)
 #' @param key your PLoS API key, either enter, or loads from .Rprofile
-#' @param callopts Optional additional curl options (debugging tools mostly)
-#' @param curl If using in a loop, call getCurlHandle() first and pass
-#'  the returned value in here (avoids unnecessary footprint)
+#' @param ... Optional additional curl options (debugging tools mostly)
+#' 
 #' @return Number of search results (vis = FALSE), or number of search in a table
 #'    and a histogram of results (vis = TRUE).
 #' @examples \dontrun{
 #' plosword('Helianthus')
-#' plosword(list('monkey','Helianthus','sunflower','protein','whale'),
-#'    vis = TRUE)
 #' out <- plosword(list('monkey','replication','design','sunflower','whale'),
 #'    vis = TRUE)
 #' out[[1]] # results in a data frame
 #' out[[2]] # results in a bar plot
 #'
 #' # Pass in curl options
-#' plosword('Helianthus', callopts=list(verbose=TRUE))
+#' plosword('Helianthus', config=verbose())
 #' }
-#' @export
+
 plosword <- function(terms, vis = FALSE,
-  key = getOption("PlosApiKey", stop("need an API key for PLoS Journals")),
-  curl = getCurlHandle(), callopts=list())
+  key = getOption("PlosApiKey", stop("need an API key for PLoS Journals")), ...)
 {
-  Term = NULL
-  No_Articles = NULL
-  url = 'http://api.plos.org/search'
+  Term=No_Articles=NULL
 
   if (length(terms) == 1) {
-    args <- list(apikey = key)
-    if(!is.na(terms))
-      args$q <- terms
-    args$fl <- "id" # just ID field to speed up return of search results
-    args$wt <- "json"
-    tt <- getForm(url,
-                  .params = args,
-                  .opts=callopts,
-                  curl = curl)
-    numres <- jsonlite::fromJSON(I(tt), FALSE)$response$numFound
-    names(numres) <- 'Number of articles with search term'
-    return(numres)
-  }
-  else {
-    search_ <- function(x) {
-      args <- list(apikey = key)
-      if(!is.na(x))
-        args$q <- x
-      args$fl <- "id" # just ID field to speed up return of search results
-      args$wt <- "json"
-      tt <- getForm(url,
-                    .params = args,
-                    .opts=callopts,
-                    curl = curl)
-      numres <- jsonlite::fromJSON(I(tt), FALSE)$response$numFound
-      return(numres)
-    }
-    temp <- ldply(terms, search_)
-    names(temp) <- "No_Articles"
+    setNames(search_(terms, key, ...), 'Number of articles with search term')
+  } else {
+    temp <- setNames(ldply(terms, search_, key=key, ...), "No_Articles")
     temp$Term <- terms
     temp$Term <- as.character(temp$Term)
     if (vis) {
@@ -71,4 +41,11 @@ plosword <- function(terms, vis = FALSE,
     }
     return( temp )
   }
+}
+
+search_ <- function(terms, key, ...){
+  args <- ploscompact(list(apikey = key, q = terms, fl="id", wt="json"))
+  tt <- GET(pbase(), query = args, ...)
+  stop_for_status(tt)
+  jsonlite::fromJSON(content(tt, "text"), FALSE)$response$numFound
 }
