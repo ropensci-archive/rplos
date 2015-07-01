f3ceaf0b14d80d62e3911653383f213d0fcfcbd2
Scott Chamberlain <myrmecocystus@gmail.com>
2015-06-30 20:09:37 -0700

    dropped startup message

--- R/startup.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(...) {
-	packageStartupMessage("\n\n New to rplos? Tutorial at http://ropensci.org/tutorials/rplos_tutorial.html. Use suppressPackageStartupMessages() to suppress these startup messages in the future\n")
-} 
